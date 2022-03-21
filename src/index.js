73da7b9b0149eedebef2cddf064928b03db912cd
tomike19 <kolajoelizabeth@gmail.com>
2022-03-21 11:37:41 -0700

    dashboard-table

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import App from './app';
+import App from './App';
 
 import 'react-toastify/dist/ReactToastify.css';
 
